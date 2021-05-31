df0687c313486d24eb659ce486c9fa312d354cfa
Mateusz Wlodarski <wlodarsm@mcmaster.ca>
2021-05-31 14:54:59 -0400

    return the reverse strand complement of a given sequence

--- reverseStrand.py
+++ reverseStrand.py
@@ -2,6 +2,7 @@
 #HELP : python reverseStrand -h
 
 import sys, argparse
+from biopack import formatFasta
 
 #Help documentation
 parser = argparse.ArgumentParser(
@@ -10,35 +11,23 @@ parser = argparse.ArgumentParser(
 parser.add_argument('f', type=str, default='', help='FILE')
 args = parser.parse_args()
 
-def formatFasta(fasta):
-    '''Remove fasta header.'''
-
-    fastaStr = ''
-    with open(fasta, 'r') as fh:
-            for line in fh.readlines()[1:]:
-                fastaStr += line
-    
-    fastaStr = fastaStr.replace('\n', '').strip()
-    return fastaStr
-
 def reverseStrand(sequence):
     '''Returns the reverse complement of a nucleotide fasta file.'''
 
     sequence = formatFasta(sequence)
-    i = 0
     complement = ''
-    while i < len(sequence):
-        if sequence[i] == 'A':
+    for i in sequence:
+        if i == 'A':
             complement += 'T'
-        elif sequence[i] == 'T':
+        elif i == 'T':
             complement += 'A'
-        elif sequence[i] == 'C':
+        elif i == 'C':
             complement += 'G'
-        elif sequence[i] == 'G':
+        elif i == 'G':
             complement += 'C'
-        i += 1
     
     return complement[::-1] #reverse direction
+
 seq = sys.argv[1]
 print(reverseStrand(seq))
 
