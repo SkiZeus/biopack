edb89a7ea310dca315982c7686f49c26f9a03502
Mateusz Wlodarski <wlodarsm@mcmaster.ca>
2021-05-31 14:52:22 -0400

    return the most frequent kmer(s) of a given size in a given sequence

--- frequentKmer.py
+++ frequentKmer.py
@@ -2,39 +2,15 @@
 #HELP : python frequentKmer -h
 
 import sys, argparse
+from biopack import formatFasta, kmerCount
 
 #Help documentation
 parser = argparse.ArgumentParser(
     description='Given a fasta file, return the most frequent kmer(s) of length s.',
     epilog='Written by Mateusz Wlodarski, https://github.com/Zeus-ski')
-parser.add_argument('f', type=str, default='', help='FILE')
-parser.add_argument('size', type=int, default=0, help='"SIZE"')
+parser.add_argument('file', type=str, default='', help='FILE')
+parser.add_argument('kmerSize', type=int, default=0, help='"SIZE"')
 args = parser.parse_args()
-
-def formatFasta(fasta):
-    '''Helper function which takes in a .fa file and returns the DNA sequence as a string, without any new line '\n'  or non nucleotide characters.'''
-
-    fastaStr = ''
-    with open(fasta, 'r') as fh:
-            for line in fh.readlines()[1:]:
-                fastaStr += line
-    
-    fastaStr = fastaStr.replace('\n', '').strip()
-    return fastaStr
-
-def kmerCount(sequence, kmer):
-    '''return the number of times a kmer appears in a given sequence. kmers cannot overlap.'''
-
-    count = 0
-    i = 0
-    while i < len(sequence):
-        if sequence[i : i + len(kmer)] == kmer:
-            count += 1
-            i += len(kmer)
-        else :
-            i += 1
-    
-    return count
 
 def frequentKmer(sequence, size):
     '''Given a fasta sequence, return the most frequent kmer of length size.'''
