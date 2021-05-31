566266b3debb2e0c2a9fb793e3b248c7693bc21d
Mateusz Wlodarski <wlodarsm@mcmaster.ca>
2021-05-31 14:50:59 -0400

    return the number of times a kmer appears in a given sequence

--- kmerCount.py
+++ kmerCount.py
@@ -2,26 +2,16 @@
 #HELP : python kmerCount -h
 
 import sys, argparse
+from biopack import formatFasta
 
 #Help documentation
 parser = argparse.ArgumentParser(
     description='Return the number of times a kmer appears in a fasta sequence. Kmers cannot overlap.',
     epilog='Written by Mateusz Wlodarski, https://github.com/Zeus-ski')
-parser.add_argument('f', type=str, default='', help='FILE')
-parser.add_argument('k', type=str, default='', help='"KMER"')
+parser.add_argument('file', type=str, default='', help='FILE')
+parser.add_argument('kmer', type=str, default='', help='"KMER"')
 args = parser.parse_args()
 
-def formatFasta(fasta):
-    '''Helper function which takes in a .fa file and returns the DNA sequence as a string, without any new line '\n'  or non nucleotide characters.'''
-
-    fastaStr = ''
-    with open(fasta, 'r') as fh:
-            for line in fh.readlines()[1:]:
-                fastaStr += line
-    
-    fastaStr = fastaStr.replace('\n', '').strip()
-    return fastaStr
-    
 def kmerCount(sequence, kmer):
     '''return the number of times a kmer appears in a given sequence. kmers cannot overlap.'''
 
@@ -36,5 +26,6 @@ def kmerCount(sequence, kmer):
             i += 1
     
     return count
+
 seq, kmer = sys.argv[1], sys.argv[2]
 print(kmerCount(seq, kmer))
