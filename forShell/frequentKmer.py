e1306b0ff91200a574d8ecf905f4111cc1eaa836
Mateusz Wlodarski <wlodarsm@mcmaster.ca>
2021-06-23 13:45:48 -0400

    improved algorithm efficiency

--- forShell/frequentKmer.py
+++ forShell/frequentKmer.py
@@ -4,31 +4,35 @@
 import sys, argparse
 from biopack import formatFasta, kmerCount
 
-#Help documentation
+'''#Help documentation
 parser = argparse.ArgumentParser(
     description='Given a fasta file, return the most frequent kmer(s) of length s.',
     epilog='Written by Mateusz Wlodarski, https://github.com/Zeus-ski')
 parser.add_argument('file', type=str, default='', help='FILE')
 parser.add_argument('kmerSize', type=int, default=0, help='"SIZE"')
-args = parser.parse_args()
+args = parser.parse_args()'''
 
 def frequentKmer(sequence, size):
     '''Given a fasta sequence, return the most frequent kmer of length size.'''
 
     sequence = formatFasta(sequence)
-    kmers = [sequence[n : n + size] for n in range(len(sequence) - size + 1)]
-    kmers = list(set(kmers))
+    kmers = {}
     
-    frequency = []
-    for i in kmers:
-        frequency.append((i, kmerCount(sequence, i)))
-   
-   
-    best = max([frequency[i][1] for i in range(len(frequency))])
+    for i in range(len(sequence) - size):
+        kmer = sequence[i : i + size]
+        if kmer in kmers:
+            kmers[kmer] += 1
+        else :
+            kmers[kmer] = 0
+    
+    best = max(kmers.values())
     result = ''
-    for i in frequency:
-        if i[1] == best:
-            result += f'{i[0]} \n'
+
+    for kmer in kmers.keys():
+        if kmers[kmer] == best:
+            result += kmer
+            result += '\n'
+    result.strip()
 
     result = f'most frequent kmer(s): \n{result}appear(s) {best} times'
     
